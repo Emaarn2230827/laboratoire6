47da46d403dbdcfe27124894c96e557832611d8a
Arnold Ema A Ghomfo <2230827@etu.cegepjonquiere.ca>
2023-03-18 21:39:31 -0400

    test de mon git

---
+++ ChatEtChien/ChatEtChien/Chat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatNamespace
+{
+    enum Humeur
+    {
+        bonne,
+        mauvaise
+    }
+    internal class Chat
+    {
+        string nom;
+        Humeur humeur;      
+        
+        
+
+    }
+}
